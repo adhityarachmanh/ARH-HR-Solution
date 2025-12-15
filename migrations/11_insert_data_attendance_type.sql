8a35c49bcbc6382eef34db359025f28dfe9d8873
adhityarachmanh <adhityarachmanh@gmail.com>
2025-12-15 11:18:11 +0700

    up

---
+++ migrations/11_insert_data_attendance_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO "AttendanceTypes" ("AttendanceTypeCode", "AttendanceTypeName", "CreatedDate", "CreatedBy", "UpdatedDate", "UpdatedBy")
+VALUES ('H', 'Hadir', NOW(), 'System', NOW(), 'System');
+
+INSERT INTO "AttendanceTypes" ("AttendanceTypeCode", "AttendanceTypeName", "CreatedDate", "CreatedBy", "UpdatedDate", "UpdatedBy")
+VALUES ('TLT', 'Terlambat', NOW(), 'System', NOW(), 'System');
